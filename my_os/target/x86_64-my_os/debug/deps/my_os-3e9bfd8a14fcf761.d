77306fe6cb9eaab68f1e3f4fd144e89fccfeb90d
Ged <120242971+GedK0tlet@users.noreply.github.com>
2025-03-01 22:49:55 +0300

    cargo run - запустит в qemu эмуляторе мою ось , где должно быть написано Hello world светло синим цветом

---
+++ my_os/target/x86_64-my_os/debug/deps/my_os-3e9bfd8a14fcf761.d
@@ -0,0 +1,5 @@
+/Users/evgenii/Desktop/Develop/Rust/LerRust/ssssex/my_os/target/x86_64-my_os/debug/deps/my_os-3e9bfd8a14fcf761: src/main.rs
+
+/Users/evgenii/Desktop/Develop/Rust/LerRust/ssssex/my_os/target/x86_64-my_os/debug/deps/my_os-3e9bfd8a14fcf761.d: src/main.rs
+
+src/main.rs:
---
+++ my_os/target/x86_64-my_os/debug/deps/rustc_std_workspace_core-b33d10d792bf315b.d
@@ -0,0 +1,7 @@
+/Users/evgenii/Desktop/Develop/Rust/LerRust/ssssex/my_os/target/x86_64-my_os/debug/deps/librustc_std_workspace_core-b33d10d792bf315b.rmeta: /Users/evgenii/.rustup/toolchains/nightly-aarch64-apple-darwin/lib/rustlib/src/rust/library/rustc-std-workspace-core/lib.rs
+
+/Users/evgenii/Desktop/Develop/Rust/LerRust/ssssex/my_os/target/x86_64-my_os/debug/deps/librustc_std_workspace_core-b33d10d792bf315b.rlib: /Users/evgenii/.rustup/toolchains/nightly-aarch64-apple-darwin/lib/rustlib/src/rust/library/rustc-std-workspace-core/lib.rs
+
+/Users/evgenii/Desktop/Develop/Rust/LerRust/ssssex/my_os/target/x86_64-my_os/debug/deps/rustc_std_workspace_core-b33d10d792bf315b.d: /Users/evgenii/.rustup/toolchains/nightly-aarch64-apple-darwin/lib/rustlib/src/rust/library/rustc-std-workspace-core/lib.rs
+
+/Users/evgenii/.rustup/toolchains/nightly-aarch64-apple-darwin/lib/rustlib/src/rust/library/rustc-std-workspace-core/lib.rs:
